2b1a30fd4b830b86b2189d2e7c59682fd50b1084
Jerry Brown <150794921+VastEpiphany@users.noreply.github.com>
2024-01-18 20:28:36 +0800

    Add files via upload
    注意：1.目前自动机器学习使用的评估方法仍为MASE，明天可能需要再改一下
                2.然后目前计算了日线的closing price和volume数据，目前设置的是预测时间点后48天的数据，后续可以加上时间并更改，然后具体还没跑，马上完成（调GPU调了一天...）
              3.对城楷同学的代码改了改，保存输出了两个csv，然后让时间Date也能正常输出而不是NaN

---
+++ BoxCox_Trading_Volume_Predict.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import torch
+from autogluon.timeseries import TimeSeriesDataFrame,TimeSeriesPredictor
+
+
+
+# 载入数据
+data = pd.read_csv('processed_daily_data.csv', encoding='ANSI')
+data['Date'] = pd.to_datetime(data['Date'])
+data['item_id'] = 0
+
+train_data = TimeSeriesDataFrame.from_data_frame(
+    data,
+    id_column="item_id",
+    timestamp_column="Date",
+)
+
+predictor = TimeSeriesPredictor(
+    prediction_length=48,
+    path='autogluon-HSI',
+    target='BoxCox_Trading_Volume',
+    eval_metric="MASE",
+    freq='1D'
+)
+
+predictor.fit(
+    train_data,
+    presets="best_quality",
+    time_limit = 600
+)
+
+predictions = predictor.predict(train_data)
+print(predictions)
+
+# 确保predictions是一个DataFrame
+if not isinstance(predictions, pd.DataFrame):
+    predictions = pd.DataFrame(predictions)
+
+# 保存predictions为CSV文件
+predictions.to_csv('my_BoxCox_Trading_Volume_predictions.csv', index=False)
