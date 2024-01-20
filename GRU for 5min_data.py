3f15bc0ac5af68573952c4833fd859e475674f08
Beaker-1220 <151807286+Beaker-1220@users.noreply.github.com>
2024-01-20 11:10:35 +0800

    Add files via upload

--- GRU for 5min_data.py
+++ GRU for 5min_data.py
@@ -1,4 +1,4 @@
-
+import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,6 +17,8 @@ from tensorflow.keras.layers import GRU, LSTM
 from tensorflow.keras.optimizers import Adam
 from matplotlib import pyplot
 from pickle import load
+import gc
+gc.collect()
 
 # Load data
 X_train = np.load("X_train_5.npy", allow_pickle=True)
@@ -25,19 +27,21 @@ X_test = np.load("X_test_5.npy", allow_pickle=True)
 y_test = np.load("y_test_5.npy", allow_pickle=True)
 yc_train = np.load("yc_train_5.npy", allow_pickle=True)
 yc_test = np.load("yc_test_5.npy", allow_pickle=True)
+X_val = np.load("X_val_5.npy", allow_pickle=True)
+y_val = np.load("y_val_5.npy", allow_pickle=True)
+yc_val = np.load("yc_val_5.npy", allow_pickle=True)
+test_index = np.load("test_predict_index_5.npy",allow_pickle=True)
 
 # Parameters
-LR = 0.00001
+LR = 0.0001
 BATCH_SIZE = 128
 N_EPOCH = 50
 
 input_dim = X_train.shape[1]
 feature_size = X_train.shape[2]
 output_dim = y_train.shape[1]
-print(y_test)
-print(size(y_train))
-print(input_dim)
-print(feature_size)
+real_mean = []
+predicted_mean = []
 
 def basic_GRU(input_dim, output_dim, feature_size) -> tf.keras.models.Model:
     model = Sequential()
@@ -49,7 +53,7 @@ def basic_GRU(input_dim, output_dim, feature_size) -> tf.keras.models.Model:
     # model.add(Dense(32))
     model.add(Dense(units=output_dim))
     model.compile(optimizer=Adam(lr=LR), loss='mse')
-    history = model.fit(X_train, y_train, epochs=N_EPOCH, batch_size=BATCH_SIZE, validation_data=(X_test, y_test),
+    history = model.fit(X_train, y_train, epochs=N_EPOCH, batch_size=BATCH_SIZE, validation_data=(X_val, y_val),
                         verbose=2, shuffle=False)
     pyplot.plot(history.history['loss'], label='train')
     pyplot.plot(history.history['val_loss'], label='validation')
@@ -89,74 +93,102 @@ def calculate_drawdowns(prices):
 
     return drawdowns, max_drawdown
 
-
 def plot_testdataset_result(X_test, y_test):
-
+    gc.collect()
     test_yhat = model.predict(X_test, verbose=0)
     y_scaler = load(open('y_scaler_5.pkl', 'rb'))
     test_predict_index = np.load("test_predict_index_5.npy", allow_pickle=True)
 
     rescaled_real_y = y_scaler.inverse_transform(y_test)
     rescaled_predicted_y = y_scaler.inverse_transform(test_yhat)
-
+    print("Plot test dataset result...")
     predict_result = pd.DataFrame()
+
+    predict_result_list = []
+    real_price_list = []
+
     for i in range(rescaled_predicted_y.shape[0]):
-        print("Plot test dataset result...")
+
         y_predict = pd.DataFrame(rescaled_predicted_y[i], columns=["predicted_price"],
                                  index=test_predict_index[i:i + output_dim])
-        predict_result = pd.concat([predict_result, y_predict], axis=1, sort=False)
+        predict_result_list.append(y_predict)
+        #predict_result = pd.concat([predict_result, y_predict], axis=1, sort=False)
 
     real_price = pd.DataFrame()
     for i in range(rescaled_real_y.shape[0]):
-        print("Plot test dataset result...")
-        y_train = pd.DataFrame(rescaled_real_y[i], columns=["real_price"],
+
+        y_real = pd.DataFrame(rescaled_real_y[i], columns=["real_price"],
                                index=test_predict_index[i:i + output_dim])
-        real_price = pd.concat([real_price, y_train], axis=1, sort=False)
+        real_price_list.append(y_real)
+        #real_price = pd.concat([real_price, y_real], axis=1, sort=False)
 
 
     predict_result['predicted_mean'] = predict_result.mean(axis=1)
     real_price['real_mean'] = real_price.mean(axis=1)
+
+    predict_mean = [sum(sublist) / len(sublist) for sublist in predict_result_list]
+    real_mean = [sum(sublist) / len(sublist) for sublist in real_price_list]
+
+    real_series = pd.Series(real_mean)
+    predict_series = pd.Series(predict_mean)
+
+
+
     # Plot the predicted result
     plt.figure(figsize=(16, 8))
-    plt.plot(real_price["real_mean"])
-    plt.plot(predict_result["predicted_mean"], color='r')
-    plt.xlabel("Date")
+    # plt.plot(real_price["real_mean"])
+    # plt.plot(predict_result["predicted_mean"], color='r')
+    plt.plot(test_index, real_mean)
+    plt.plot(test_index,predict_mean, color='r')
+    plt.xlabel("DateTime")
     plt.ylabel("Stock price")
     plt.legend(("Real price", "Predicted price"), loc="upper left", fontsize=16)
     plt.title("The result of Testing", fontsize=20)
     plt.show()
+    print("Plot test dataset result done!")
 
-    real_series = pd.Series( real_price['real_mean'])
-    predict_series = pd.Series(predict_result['predicted_mean'])
-    real_drawdowns, real_max_drawdown = calculate_drawdowns(real_series)
-    print("Real Max Drawdown:", real_max_drawdown)
-
-    predicted_drawdowns, predicted_max_drawdown = calculate_drawdowns(predict_series)
-    print("Predicted Max Drawdown:", predicted_max_drawdown)
-
-    # 可视化回撤
-    plt.figure(figsize=(16, 8))
-    plt.plot(real_drawdowns, label='Real Price Drawdowns')
-    plt.title("Drawdowns over Time")
-    plt.xlabel("Date")
-    plt.ylabel("Drawdown")
-    plt.legend()
-    plt.show()
-
-    # 可视化回撤
-    plt.figure(figsize=(16, 8))
-    plt.plot(real_drawdowns, label='Prediction Price Drawdowns')
-    plt.title("Drawdowns over Time")
-    plt.xlabel("Date")
-    plt.ylabel("Drawdown")
-    plt.legend()
-    plt.show()
+    #
+    # real_series = pd.Series(real_mean)
+    # predict_series = pd.Series(predict_mean)
+    # predict_result['predicted_mean'] = predict_series
+    # real_price['real_mean'] = real_series
+    # real_series = pd.Series(real_price['real_mean'])
+    # predict_series = pd.Series(predict_result['predicted_mean'])
+    #
+    # real_drawdowns, real_max_drawdown = calculate_drawdowns(real_series)
+    # print("Real Max Drawdown:", real_max_drawdown)
+    #
+    # predicted_drawdowns, predicted_max_drawdown = calculate_drawdowns(predict_series)
+    # print("Predicted Max Drawdown:", predicted_max_drawdown)
+    #
+    # # 可视化回撤
+    # plt.figure(figsize=(16, 8))
+    # plt.plot(real_drawdowns, label='Real Price Drawdowns')
+    # plt.title("Drawdowns over Time")
+    # plt.xlabel("Date")
+    # plt.ylabel("Drawdown")
+    # plt.legend()
+    # plt.show()
+    #
+    # # 可视化回撤
+    # plt.figure(figsize=(16, 8))
+    # plt.plot(real_drawdowns, label='Prediction Price Drawdowns')
+    # plt.title("Drawdowns over Time")
+    # plt.xlabel("Date")
+    # plt.ylabel("Drawdown")
+    # plt.legend()
+    # plt.show()
     # Calculate RMSE
-    predicted = predict_result["predicted_mean"]
-    real = real_price["real_mean"]
-    RMSE = np.sqrt(mean_squared_error(predicted, real))
-
+    # predicted = predict_result["predicted_mean"]
+    # real = real_price["real_mean"]
+    RMSE = sqrt(mean_squared_error(predict_mean,real_mean))
     return RMSE
+
+
+
+
 
 test_RMSE = plot_testdataset_result(X_test, y_test)
 print("----- Test_RMSE -----", test_RMSE)
+
+
